9c4e6ade5b142145ffe2bead1443f02e00889f23
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-15 17:55:29 +0530

    basic credential checking added for logging in

--- app/users.go
+++ app/users.go
@@ -4,6 +4,8 @@ import (
 	"log"
 
 	"github.com/mrtyunjaygr8/passwd/ent"
+	"github.com/mrtyunjaygr8/passwd/ent/user"
+	"github.com/mrtyunjaygr8/passwd/utils"
 )
 
 func (a *App) CreateUser(email, password string) (*ent.User, error) {
@@ -14,5 +16,21 @@ func (a *App) CreateUser(email, password string) (*ent.User, error) {
 	}
 
 	return user, nil
+}
 
+func (a *App) LoginUser(email, password string) error {
+	user, err := a.Client.User.Query().Where(user.Email(email)).Only(a.Context)
+	if err != nil {
+		if _, ok := err.(*ent.NotFoundError); ok {
+			log.Printf("user: %v not found", email)
+			return utils.NOT_FOUND
+		}
+		log.Printf("err querying user, %v, %v", email, err)
+		return err
+	}
+
+	if password != user.Password {
+		return utils.BAD_REQUEST
+	}
+	return nil
 }
