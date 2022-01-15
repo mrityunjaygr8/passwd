f2110e725f36b8c9a92a8326be8e88eb0fdf96a3
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-15 15:03:39 +0530

    config struct and viper added

---
+++ utils/config.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB_HOST string
+	DB_PORT int
+	DB_NAME string
+	DB_USER string
+	DB_PASS string
+}
+
+func GetConfig() Config {
+	viper.AddConfigPath(".")
+	viper.SetConfigFile("config.toml")
+
+	viper.SetDefault("DB_HOST", "localhost")
+	viper.SetDefault("DB_PORT", 5432)
+	viper.SetDefault("DB_NAME", "passwd")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config: %w\n", err))
+	}
+
+	var c Config
+	err = viper.Unmarshal(&c)
+	if err != nil {
+		log.Fatalf("unable to decode into struct, %v", err)
+	}
+
+	return c
+}
