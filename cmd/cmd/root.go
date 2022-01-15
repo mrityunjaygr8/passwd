b11223b72956fc8aadb21d8258a37f090e6ca217
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-15 17:06:57 +0530

    rootCmd file restructured

--- cmd/cmd/root.go
+++ cmd/cmd/root.go
@@ -1,37 +1,23 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
-	"log"
+	"os"
 
-	"github.com/mrtyunjaygr8/passwd/app"
-	"github.com/mrtyunjaygr8/passwd/utils"
 	"github.com/spf13/cobra"
 
 	_ "github.com/lib/pq"
 )
 
-var RootCmd = &cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:   "passwd",
 	Short: "CLI for password management",
 	Long:  `CLI for password management written in go.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		config := utils.GetConfig()
-		fmt.Println(config)
+}
 
-		client := app.CreateApp(config)
-		defer client.Close()
-
-		ctx := context.Background()
-		if err := client.Schema.Create(ctx); err != nil {
-			log.Fatal("failed creating schema resources: %w", err)
-		}
-
-		newUser, err := client.User.Create().SetEmail("msyt119691@gmail.com").SetPassword("dr0w.Ssap").Save(ctx)
-		if err != nil {
-			log.Println("error in creating user: %w", err)
-		}
-		fmt.Println(newUser)
-	},
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
