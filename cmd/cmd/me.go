30254eb91d93b3ff07711ac7085162df04eb49f5
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-16 15:29:29 +0530

    cmd to show the currently logged in user

---
+++ cmd/cmd/me.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/mrtyunjaygr8/passwd/app"
+	"github.com/mrtyunjaygr8/passwd/utils"
+	"github.com/spf13/cobra"
+)
+
+var me_cmd = &cobra.Command{
+	Use:   "me",
+	Short: "Get my details",
+	Long:  "Get the details of the currently logged in user",
+	Run: func(cmd *cobra.Command, args []string) {
+		config := utils.GetConfig()
+
+		app := app.CreateApp(config)
+		defer app.Client.Close()
+		out, err := app.GetUser(getToken())
+		if err != nil {
+			log.Fatal("An error has occurred", err)
+		}
+
+		fmt.Println("User info: ", out)
+	},
+}
+
+func init() {
+	root_cmd.AddCommand(me_cmd)
+}
