2d98109c064e7cbd78e173f733b2b166b63c4254
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-16 18:07:49 +0530

    creds functionality created

---
+++ app/creds.go
@@ -0,0 +1,70 @@
+package app
+
+import (
+	"log"
+
+	"github.com/mrtyunjaygr8/passwd/ent"
+	"github.com/mrtyunjaygr8/passwd/ent/creds"
+	"github.com/mrtyunjaygr8/passwd/ent/passwords"
+	"github.com/mrtyunjaygr8/passwd/utils"
+)
+
+func (a *App) ListCreds(token string) []*ent.Creds {
+	user, err := a.GetUser(token)
+	if err != nil {
+		log.Fatal("an error has occurred: ", err)
+	}
+
+	creds, err := user.QueryCreds().All(a.Context)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return creds
+}
+
+func (a *App) CreateCreds(token, name, username, password, url string) (*ent.Creds, error) {
+	user, err := a.GetUser(token)
+	if err != nil {
+		log.Fatal("an error has occurred: ", err)
+	}
+
+	cred, err := a.Client.Creds.Create().SetUsername(username).SetUser(user).SetName(name).SetURL(url).Save(a.Context)
+	if err != nil {
+		log.Printf("an error occurred while creating the new cred: %v", err)
+		return &ent.Creds{}, utils.BAD_REQUEST
+	}
+
+	_, err = a.Client.Passwords.Create().SetPassword(password).SetCred(cred).Save(a.Context)
+	if err != nil {
+		log.Printf("an error occurred while creating the new cred: %v", err)
+		return &ent.Creds{}, utils.BAD_REQUEST
+	}
+
+	return cred, nil
+}
+
+func (a *App) GetCred(token, name string) (*ent.Creds, string, error) {
+	user, err := a.GetUser(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cred, err := user.QueryCreds().Where(creds.Name(name)).Only(a.Context)
+	if err != nil {
+		if _, ok := err.(*ent.NotFoundError); ok {
+			log.Printf("cred: %v not found", name)
+			return &ent.Creds{}, "", utils.NOT_FOUND
+		}
+		log.Printf("err querying cred, %v, %v", name, err)
+		return &ent.Creds{}, "", err
+
+	}
+
+	pass, _ := cred.QueryPasswords().Order(ent.Desc(passwords.FieldCreateTime)).Limit(1).All(a.Context)
+	if err != nil {
+		log.Println(err)
+		return &ent.Creds{}, "", utils.NOT_FOUND
+
+	}
+	return cred, pass[0].Password, nil
+}
