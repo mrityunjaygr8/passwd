ea7004140b14f11138f37ca83c51fb39cae00fc0
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-17 14:52:42 +0530

    default password length and disable special values set in constants.go

--- utils/constants.go
+++ utils/constants.go
@@ -3,3 +3,6 @@ package utils
 const LOGIN_FILE = "/tmp/passwd"
 
 var SYMMETRIC_KEY = []byte("YELLOW SUBMARINE, BLACK WIZARDRY")
+
+const DEFAULT_PASS_LENGTH = 14
+const DEFAULT_DISABLE_SPECIAL = false
