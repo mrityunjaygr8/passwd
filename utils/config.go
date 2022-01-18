4ac8d7ad39ed3123bb1ed32612579f0e22922742
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-01-18 13:07:51 +0530

    handling heroku db url env

--- utils/config.go
+++ utils/config.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -27,6 +28,7 @@ func GetConfig() Config {
 	viper.BindEnv("db_pass")
 	viper.BindEnv("port")
 	viper.BindEnv("host")
+	viper.BindEnv("database_url")
 	viper.AddConfigPath(".")
 	viper.SetConfigFile("config.toml")
 
@@ -41,8 +43,24 @@ func GetConfig() Config {
 		if ok := strings.Contains(err.Error(), "no such file or directory"); ok {
 			u := viper.Get("DB_USER")
 			p := viper.Get("DB_PASS")
-			if u == nil || p == nil {
+
+			// This is for heroku only
+			d := viper.Get("DATABASE_URL")
+			// fmt.Println(fmt.Sprint(d))
+			if (u == nil || p == nil) && d == nil {
 				panic(fmt.Errorf("Fatal error config: %w\n", fmt.Errorf("You have not specified a config file or used envs for DB_USER and DB_PASS")))
+			}
+
+			// This is for heroku only
+			if d != nil {
+				// r, _ := regexp.Compile(`postgres://([\w]+):([\w]+)@([\w]+):([\w]+)/([\w]+)`)
+				r, _ := regexp.Compile(`postgres://(?P<user>[\w]+):(?P<pass>[\w]+)@(?P<host>[\w-.]+):(?P<port>[\d]+)/(?P<name>[\w]+)`)
+				matches := r.FindSubmatch([]byte(fmt.Sprint(d)))
+				viper.Set("DB_NAME", string(matches[1]))
+				viper.Set("DB_PASS", string(matches[2]))
+				viper.Set("DB_HOST", string(matches[3]))
+				viper.Set("DB_PORT", string(matches[4]))
+				viper.Set("DB_NAME", string(matches[5]))
 			}
 		} else {
 			// Config file was found but another error was produced
@@ -55,6 +73,8 @@ func GetConfig() Config {
 	if err != nil {
 		log.Fatalf("unable to decode into struct, %v", err)
 	}
+
+	fmt.Println(c)
 
 	return c
 }
